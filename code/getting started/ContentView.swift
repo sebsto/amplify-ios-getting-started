f544f5ea86ae9213ab558b5e0b05c4f0301bf330
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2020-10-23 07:49:36 +0200

    move user data in a separate file

--- code/getting started/ContentView.swift
+++ code/getting started/ContentView.swift
@@ -8,63 +8,6 @@
 
 import SwiftUI
 
-// singleton object to store user data
-class UserData : ObservableObject {
-    private init() {}
-    static let shared = UserData()
-    
-    @Published var notes      : [Note] = []
-    @Published var isSignedIn : Bool   = false
-}
-
-class Note : Identifiable, ObservableObject {
-    var id          : String
-    var name        : String
-    var description : String?
-    var imageName   : String?
-    @Published var image : Image?
-        
-    init(id: String, name: String, description: String? = nil, image: String? = nil ) {
-        self.id          = id
-        self.name        = name
-        self.description = description
-        self.imageName   = image
-    }
-
-    convenience init(from data: NoteData) {
-        self.init(id: data.id, name: data.name, description: data.description, image: data.image)
-        
-        if let name = self.imageName {
-            // asynchronously download the image
-            Backend.shared.retrieveImage(name: name) { (data) in
-                DispatchQueue.main.async() {
-                    let uim = UIImage(data: data)
-                    self.image = Image(uiImage: uim!)
-                }
-            }
-        }
-        // store API object for easy retrieval later
-        self._data = data
-    }
-    
-    fileprivate var _data : NoteData?
-    
-    // access the privately stored NoteData or build one if we don't have one.
-    var data : NoteData {
-
-        if (_data == nil) {
-
-            _data = NoteData(id: self.id,
-                             name: self.name,
-                             description: self.description,
-                             image: self.imageName)
-        }
-        
-        return _data!
-    }
-    
-}
-
 struct ListRow: View {
     @ObservedObject var note : Note
     var body: some View {
