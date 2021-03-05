f5f0da5158ac34127e1f8ea3d7bac62309d4dc7c
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2021-03-05 18:41:07 +0100

    fix AWS_CLI path

--- code/cli-build/codebuild-configuration.sh
+++ code/cli-build/codebuild-configuration.sh
@@ -3,6 +3,8 @@
 ## My project and environment specific values
 ## Replace all of these with yours 
 
+export LANG=en_US.UTF-8
+AWS_CLI=/usr/local/bin/aws
 REGION=$(curl -s 169.254.169.254/latest/meta-data/placement/region/)
 
 # My secret ARNs, you get them from import_secrets.sh
@@ -15,16 +17,16 @@ S3_APPLE_DISTRIBUTION_CERT_SECRET=apple-dist-certificate
 S3_MOBILE_PROVISIONING_PROFILE_SECRET=amplify-getting-started-provisionning
 
 # 3. get the secrets at build
-AMPLIFY_APPID=$(aws --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_APPID_SECRET --query SecretString --output text)
-AMPLIFY_PROJECT_NAME=$(aws --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_PROJECT_NAME_SECRET --query SecretString --output text)
-AMPLIFY_ENV=$(aws --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_ENV_SECRET --query SecretString --output text)  
+AMPLIFY_APPID=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_APPID_SECRET --query SecretString --output text)
+AMPLIFY_PROJECT_NAME=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_PROJECT_NAME_SECRET --query SecretString --output text)
+AMPLIFY_ENV=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $AMPLIFY_ENV_SECRET --query SecretString --output text)  
 
-APPLE_ID=$(aws --region $REGION secretsmanager get-secret-value --secret-id $APPLE_ID_SECRET --query SecretString --output text)
-APPLE_SECRET=$(aws --region $REGION secretsmanager get-secret-value --secret-id $APPLE_SECRET_SECRET --query SecretString --output text)
+APPLE_ID=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_ID_SECRET --query SecretString --output text)
+APPLE_SECRET=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $APPLE_SECRET_SECRET --query SecretString --output text)
 
 # These are base64 values, we will need to decode to a file when needed
-S3_APPLE_DISTRIBUTION_CERT=$(aws --region $REGION secretsmanager get-secret-value --secret-id $S3_APPLE_DISTRIBUTION_CERT_SECRET --query SecretBinary --output text)
-S3_MOBILE_PROVISIONING_PROFILE=$(aws --region $REGION secretsmanager get-secret-value --secret-id $S3_MOBILE_PROVISIONING_PROFILE_SECRET --query SecretBinary --output text)
+S3_APPLE_DISTRIBUTION_CERT=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $S3_APPLE_DISTRIBUTION_CERT_SECRET --query SecretBinary --output text)
+S3_MOBILE_PROVISIONING_PROFILE=$($AWS_CLI --region $REGION secretsmanager get-secret-value --secret-id $S3_MOBILE_PROVISIONING_PROFILE_SECRET --query SecretBinary --output text)
 
 SCHEME="getting started"
 CONFIGURATION="Release"
