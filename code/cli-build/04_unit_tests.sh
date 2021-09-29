b7b8073186a3e444d4831d71b1bc9c342ace1301
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2021-09-29 22:32:52 +0200

    fix test script

--- code/cli-build/04_unit_tests.sh
+++ code/cli-build/04_unit_tests.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+
+AWS_CLI=/usr/local/bin/aws
+REGION=$(curl -s 169.254.169.254/latest/meta-data/placement/region/)
+HOME=/Users/ec2-user
+
+pushd $HOME/amplify-ios-getting-started/code
 
 WORKSPACE="getting started.xcworkspace"
 SCHEME="getting started"
@@ -6,6 +12,6 @@ PHONE_MODEL="iPhone 13"
 IOS_VERSION="15.0"
 
 xcodebuild test \
-    -workspace $WORKSPACE \
-    -scheme $SCHEME        \
+    -workspace "$WORKSPACE" \
+    -scheme "$SCHEME"       \
     -destination platform="iOS Simulator",name="${PHONE_MODEL}",OS=${IOS_VERSION} >> /Users/ec2-user/log/unit_tests.log 2>&1
