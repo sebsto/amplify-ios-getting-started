7aac4c0d06dbe3a6c0511312254f5b6685003b95
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2021-10-27 10:27:58 +0200

    add import sceret for TEST provisioning

--- code/cli-build/import_secrets.sh
+++ code/cli-build/import_secrets.sh
@@ -10,5 +10,6 @@ aws --region $REGION secretsmanager create-secret --name apple-signing-dev-certi
 aws --region $REGION secretsmanager create-secret --name apple-signing-dist-certificate --secret-binary fileb://./secrets/apple_dist_seb.p12 
 aws --region $REGION secretsmanager create-secret --name amplify-getting-started-dist-provisionning --secret-binary fileb://./secrets/getting-started-ios-dist.mobileprovision
 aws --region $REGION secretsmanager create-secret --name amplify-getting-started-dev-provisionning --secret-binary fileb://./secrets/getting-started-ios-dev.mobileprovision
+aws --region $REGION secretsmanager create-secret --name amplify-getting-started-test-provisionning --secret-binary fileb://./secrets/gettingstartediosdistuitestrunner.mobileprovision
 aws --region $REGION secretsmanager create-secret --name apple-id --secret-string myemail@me.com
 aws --region $REGION secretsmanager create-secret --name apple-secret --secret-string aaaa-aaaa-aaaa-aaaa 
