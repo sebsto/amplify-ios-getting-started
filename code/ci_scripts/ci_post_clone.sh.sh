3fb9be8af99e2c0c1bb47ceb0e50438dc4e6ce2d
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-16 21:29:09 +0200

    rename file to match conventions

--- code/ci_scripts/ci_post_clone.sh.sh
+++ code/ci_scripts/ci_post_clone.sh.sh
@@ -6,5 +6,10 @@
 #  Created by Stormacq, Sebastien on 16/09/2022.
 #  Copyright © 2022 Stormacq, Sebastien. All rights reserved.
 
+uname -a
+ls -al /
+cat /etc/motd
+ps -ax
+
 echo "ci_post_clone - done"
 exit 0
