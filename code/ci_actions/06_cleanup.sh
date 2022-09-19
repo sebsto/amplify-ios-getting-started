0a9ad802504673ffd323b75be3f40907be5f3140
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2022-09-19 07:40:21 +0200

    add a cleanup script to delete secrets

---
+++ code/ci_actions/06_cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+
+# delete build secrets 
+echo "Delete certificates directory"
+rm -rf ~/certificates 
+
+KEYCHAIN_NAME=dev.keychain
+if [ -f $HOME/Library/Keychains/"${KEYCHAIN_NAME}"-db ]; then
+    echo "Deleting old ${KEYCHAIN_NAME} keychain"
+    security delete-keychain "${KEYCHAIN_NAME}"
+fi
+
+# no need to delete the system keychain
+# it only contains the Apple WWDR public certificate
