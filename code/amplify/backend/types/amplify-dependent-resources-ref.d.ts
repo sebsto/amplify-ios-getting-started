c6d1f0876738f3e628e85d0c2581f7caf0fd52b5
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2023-02-07 12:15:48 +0100

    update xcode proj to xcode 14.2

--- code/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ code/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,7 +9,8 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolArn": "string",
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
-            "AppClientID": "string"
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
         }
     },
     "api": {
