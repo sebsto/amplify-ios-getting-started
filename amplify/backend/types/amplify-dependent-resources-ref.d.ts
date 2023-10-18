9dfe83681f695d581c6d57f9cb4ddba93874c347
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2023-10-17 20:19:31 -0500

    refresh for latest amplify cli

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "iosgettingstarted": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "iosgettingstarted2ee74a53": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "image": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
